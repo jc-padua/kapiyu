e3529671a6783e5a2c15cf78c7f30eb3e9c74767
Jork Cedric <jcp093001@gmail.com>
2022-12-20 22:44:28 +0800

    wtf and flash message added

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "bigwhitepotato"
+
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/user/<name>')
@@ -13,6 +22,16 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit:
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully!")
+    return render_template('name.html', name=name, form=form)
 
 if __name__ == '__main__':
     app.run(host='localhost', debug=True)
